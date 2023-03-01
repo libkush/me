f93840d75595d4de01c50aae6c63e700b313476e
Kush Patel <kush@kush.in>
2023-03-01 21:37:53 +0530

    fixed padding in posts page for mobile

--- src/routes/(root)/posts/[[page]]/+page.svelte
+++ src/routes/(root)/posts/[[page]]/+page.svelte
@@ -27,10 +27,10 @@
 
 <div class="flex flex-col h-screen flex-grow w-full sm:m-auto gap-8">
   <section class="w-full h-full">
-    <div class="posts pt-6 overflow-x-hidden max-h-full flex flex-col">
+    <div class="posts p-6 pt-16 overflow-x-hidden max-h-full flex flex-col">
       {#each posts as post}
         <article
-          class="md:border-l pb-8 pt-8 md:pl-6 md:border-zinc-100/40 w-3/5 m-auto grid items-start grid-cols-4 gap-8"
+          class="md:border-l pb-8 pt-8 md:pl-6 md:border-zinc-100/40 md:w-3/5 m-auto grid items-start grid-cols-4 gap-8"
         >
           <PostDate class="flex-col hidden md:flex text-sm" {post} decorate />
           <div class="col-span-4 md:col-span-3">
