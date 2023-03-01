f70b749bc027ad91e572b8ec2e9fe24887121a3a
Kush Patel <kush@kush.in>
2023-03-01 21:29:42 +0530

    fixed blog mobile screen problem

--- src/routes/(blog)/+layout.svelte
+++ src/routes/(blog)/+layout.svelte
@@ -8,10 +8,10 @@
 
 <Menu />
 <div class="flex flex-col min-h-screen ">
-  <div class="flex flex-col flex-grow w-full px-4 py-2">
-    <header class="flex items-center justify-between w-full max-w-2xl py-4 mx-auto lg:pb-8">
+  <div class="flex flex-col flex-grow w-full px-4 py-16 md:py-2">
+    <header class="flex items-center  w-full max-w-2xl py-4 mx-auto lg:pb-8">
       <a
-        class="text-lg font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-cyan-400"
+        class="text-lg text-center font-bold sm:text-2xl !text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-cyan-400"
         href="/"
       >
         {name}
