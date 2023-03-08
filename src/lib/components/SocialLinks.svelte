73ea1dcf41eedb38a0f16f8e30ac85c33a64c4a6
Kush Patel <kush@kush.in>
2023-03-08 15:02:30 +0530

    added aria labels to social links

--- src/lib/components/SocialLinks.svelte
+++ src/lib/components/SocialLinks.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import { discordID, email, githubUsername } from '$lib/info';
+  import { discordID, email, firstName, githubUsername } from '$lib/info';
   import { GitHubIcon, MailIcon, ChatLinesIcon, DiscordIcon } from '@indaco/svelte-iconoir';
 </script>
 
 <div class="flex flex-row gap-6 m-8">
   <ChatLinesIcon class="social" />
-  <a href="https://github.com/{githubUsername}">
+  <a href="https://github.com/{githubUsername}" aria-label="Open {firstName}'s Github Profile">
     <GitHubIcon class="social" />
   </a>
-  <a href="https://discordapp.com/users/{discordID}">
+  <a href="https://discordapp.com/users/{discordID}" aria-label="Open {firstName}'s Discord">
     <DiscordIcon class="social" />
   </a>
-  <a href="mailto:{email}">
+  <a href="mailto:{email}" aria-label="Send an E-mail to {firstName}">
     <MailIcon class="social" />
   </a>
 </div>
