ecefebd3039995f9112b559794eb203d5820acf6
Kush Patel <kush@kush.in>
2023-03-08 12:55:22 +0530

    social links now functional

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -1,6 +1,9 @@
 export const firstName = 'Kush';
 export const lastName = 'Patel';
 export const name = `${firstName} ${lastName}`;
+export const email = 'kush@kush.in';
+export const githubUsername = 'Quat3rnion';
+export const discordID = '788684047894839307';
 export const website = 'https://www.kush.in/';
 export const avatar = '/avatar.jpg';
 export const bio = `I'm ${firstName}, a student from India. I'm a full-stack developer working with Node.js, Deno, Flask, React, and Svelte. I'm learning new technologoies and building projects. I also have an interest in cyber-security and hacking. But above all, I hate describing myself, so that's it :)`;
