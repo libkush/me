4642814686643858a1f6a4987cd32769c90384e1
Kush Patel <kush@kush.in>
2023-03-08 12:02:15 +0530

    removed getAllPosts function

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -8,6 +8,7 @@ if (browser) {
 
 export async function getPosts(
   page: number,
+  all: boolean,
   before: string,
   after: string,
   during: string,
@@ -43,69 +44,50 @@ export async function getPosts(
       next: allPosts[index - 1],
       previous: allPosts[index + 1]
     }));
-  const publishedPosts = posts
-    .filter((post) => post.published)
-    .slice((page - 1) * 10, page * 10 - 1);
+
+  const publishedPosts = posts.filter((post) => post.published);
   if (before !== '') {
-    return publishedPosts.filter(
-      (post) => new Date(post.date).getTime() < new Date(before).getTime()
+    return paginate(
+      publishedPosts.filter((post) => new Date(post.date).getTime() < new Date(before).getTime()),
+      page
     );
   }
+
+  if (all) {
+    return publishedPosts;
+  }
+
   if (after !== '') {
-    return publishedPosts.filter(
-      (post) => new Date(post.date).getTime() > new Date(after).getTime()
+    return paginate(
+      publishedPosts.filter((post) => new Date(post.date).getTime() > new Date(after).getTime()),
+      page
     );
   }
   if (during !== '') {
-    return publishedPosts.filter(
-      (post) =>
-        post.metaDate === during ||
-        (new Date(post.date).getTime() >= new Date(during).getTime() &&
-          new Date(post.date).getTime() <= new Date(during).getTime() + 1000 * 60 * 60 * 24)
+    return paginate(
+      publishedPosts.filter(
+        (post) =>
+          post.metaDate === during ||
+          (new Date(post.date).getTime() >= new Date(during).getTime() &&
+            new Date(post.date).getTime() <= new Date(during).getTime() + 1000 * 60 * 60 * 24)
+      ),
+      page
     );
   }
   if (tags && tags.length > 0) {
-    return publishedPosts.filter((post) => post.tags?.some((tag) => tags.includes(tag)));
+    return paginate(
+      publishedPosts.filter((post) => post.tags?.some((tag) => tags.includes(tag))),
+      page
+    );
   }
-  return publishedPosts;
-}
-
-export async function getAllPosts(): Promise<App.BlogPostWithNextAndPrevious[]> {
-  const modules = import.meta.glob('/posts/**/*.{md,svx,svelte.md}');
-  const entries = Object.entries(modules) as [string, () => Promise<App.MdsvexFile>][];
-  const awaitingPosts = entries.map(async ([path, resolver]) => {
-    const post = await resolver();
-    const html = parse((post as any).default.render().html);
-    const preview = post.metadata.preview ? parse(post.metadata.preview) : html.querySelector('p');
-    return {
-      slug: path
-        .replace(/(\/index)?\.md/, '')
-        .split('/')
-        .pop(),
-      isIndex: path.endsWith('/index.md'),
-      date: post.metadata.date
-        ? format(addTimezoneOffset(new Date(post.metadata.date)), 'yyyy-MM-dd')
-        : undefined,
-      metaDate: post.metadata.date,
-      preview: {
-        html: preview?.toString(),
-        text: preview?.structuredText ?? preview?.toString()
-      },
-      ...(post as App.MdsvexFile).metadata
-    } as App.BlogPost;
-  });
-  const posts = (await Promise.all(awaitingPosts))
-    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-    .map((post, index, allPosts) => ({
-      ...post,
-      next: allPosts[index - 1],
-      previous: allPosts[index + 1]
-    }));
-  const publishedPosts = posts.filter((post) => post.published);
-  return publishedPosts;
+  return paginate(publishedPosts, page);
 }
 
 function addTimezoneOffset(date: Date) {
   const offsetInMilliseconds = new Date().getTimezoneOffset() * 60 * 1000;
   return new Date(new Date(date).getTime() + offsetInMilliseconds);
+}
+
+function paginate(posts: App.BlogPostWithNextAndPrevious[], page: number) {
+  return posts.slice((page - 1) * 10, page * 10 - 1);
 }
--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,18 +1,14 @@
 import type { PageServerLoad } from './$types';
-import { getAllPosts, getPosts } from '$lib/posts';
+import { getPosts } from '$lib/posts';
 import { json } from '@sveltejs/kit';
 
 export const GET = (async ({ url }) => {
   const page = url.searchParams.get('page') || '1';
-  const all = url.searchParams.get('all') || 'false';
+  const all = url.searchParams.get('all') === 'true' ? true : false;
   const after = url.searchParams.get('after') || '';
   const before = url.searchParams.get('before') || '';
   const during = url.searchParams.get('during') || '';
   const tags = url.searchParams.get('tags')?.split(',') || [];
-  if (all === 'true') {
-    const posts = await getAllPosts();
-    return json({ posts: posts });
-  }
-  const posts = await getPosts(parseInt(page), before, after, during, tags);
+  const posts = await getPosts(parseInt(page), all, before, after, during, tags);
   return json({ posts: posts });
 }) satisfies PageServerLoad;
